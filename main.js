4128429ed463f6b98465f53498d2c77c5da584d8
S77S-lab <svpcz@hotmail.com>
2019-10-16 15:22:19 +0200

    update main

--- main.js
+++ main.js
@@ -16,20 +16,20 @@ window.onclick = function(event) {
   }
 /* view posts */
 function viewPostGuide() {
-  var post = document.getElementsByClassName("page-post__guide");
-  var text = document.getElementsByClassName("page-post__text__guide");
-  post[0].classList.toggle("show__post");
-  text[0].classList.toggle("show__post__text");
+  var postView = document.getElementsByClassName("page-post__guide");
+  var textView = document.getElementsByClassName("page-post__text__guide");
+  postView[0].classList.toggle("show__post");
+  textView[0].classList.toggle("show__post__text");
 }
 function viewPostTickets() {
-  var post = document.getElementsByClassName("page-post__tickets");
-  var text = document.getElementsByClassName("page-post__text__tickets");
-  post[0].classList.toggle("show__post");
-  text[0].classList.toggle("show__post__text");
+  var postView = document.getElementsByClassName("page-post__tickets");
+  var textView = document.getElementsByClassName("page-post__text__tickets");
+  postView[0].classList.toggle("show__post");
+  textView[0].classList.toggle("show__post__text");
 }
 function viewPostDressing() {
-  var post = document.getElementsByClassName("page-post__dressing");
-  var text = document.getElementsByClassName("page-post__text__dressing");
-  post[0].classList.toggle("show__post");
-  text[0].classList.toggle("show__post__text");
+  var postView = document.getElementsByClassName("page-post__dressing");
+  var textView = document.getElementsByClassName("page-post__text__dressing");
+  postView[0].classList.toggle("show__post");
+  textView[0].classList.toggle("show__post__text");
 }
