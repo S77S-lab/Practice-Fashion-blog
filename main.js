747cb87868b389c6e1a6d5ae0bf676b134555a2a
S77S-lab <svpcz@hotmail.com>
2019-10-20 19:57:52 +0200

    update time stamp

--- main.js
+++ main.js
@@ -13,11 +13,38 @@ window.onclick = function(event) {
         }
       }
     }
-  }
+  };
 /* change cursor */
 function changeCursorFunction(){
   document.getElementById("page-title__author").style = "cursor:pointer";
 }
+/* time stamp function */
+window.onload = function() {
+  countUpFromTime("Oct 19, 2019 09:00:00", "countup");
+};
+function countUpFromTime(countFrom, countup) {
+  countFrom = new Date(countFrom).getTime();
+  var now = new Date(),
+      countFrom = new Date(countFrom),
+      timeDifference = (now - countFrom);
+    
+  var secondsInADay = 60 * 60 * 1000 * 24,
+      secondsInAHour = 60 * 60 * 1000;
+    
+  days = Math.floor(timeDifference / (secondsInADay) * 1);
+  hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+  mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+  secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+
+  var idEl = document.getElementById(countup);
+  idEl.getElementsByClassName("page-title-timeel__days")[0].innerHTML = days;
+  idEl.getElementsByClassName("page-title-timeel__hours")[0].innerHTML = hours;
+  clearTimeout(countUpFromTime.interval);
+  countUpFromTime.interval = setTimeout(function(){
+    countUpFromTime(countFrom, countup);
+  }, 1000);
+}
+
 /* view posts */
 function viewPostGuide() {
   var postView = document.getElementsByClassName("page-post__guide");
